1f91dfe4a3d8b52f173d585fb84c4f6906c78846
Zrokool123 <Zrokool123@gmail.com>
2016-11-06 16:48:49 -0700

    getting there
    need to figure out how to overide fullpage styles when resizing.

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -4,6 +4,18 @@
 
 $(document).ready(function(){
 	$('#fullpage').fullpage();
-	
 
+	$('#fullpage').removeAttr('style').attr('style', 'height: 100%; position: relative; transform: translate3d(0px, 0px, 0px)');
+	$('.section').removeAttr('style');
+	$('.fp-tableCell').removeAttr('style');
+	$('.fp-section').removeAttr('style');
+	$('.fp-completley').removeAttr('style');
+
+	$(window).resize(function(){
+		$('#fullpage').removeAttr('style').attr('style', 'height: 100%; position: relative; transform: translate3d(0px, 0px, 0px)');
+		$('.section').removeAttr('style');
+		$('fp-tableCell').removeAttr('style');
+		$('.fp-section').removeAttr('style');
+		$('.fp-completley').removeAttr('style');
+	});
 }); //////////////////////////////////////////////////////end document ready...
