521f57ae740362b4a0346c164204aac9dc2eccab
Zrokool123 <Zrokool123@gmail.com>
2016-11-07 15:05:17 -0700

    fml

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -3,23 +3,5 @@
 /*jquery here */
 
 $(document).ready(function(){
-	var $window = $(window);
-
-
-	$('#fullpage').fullpage();
-
-	$('#fullpage').removeAttr('style').attr('style', 'height: 100%; position: relative; transform: translate3d(0px, 0px, 0px)');
-	$('.section').removeAttr('style');
-	$('.fp-tableCell').removeAttr('style');
-	$('.fp-section').removeAttr('style');
-	$('.fp-completley').removeAttr('style');
-
-	$(window).resize(function(){
-
-		$('#fullpage').removeAttr('style').attr('style', 'height: 100%; position: relative; transform: translate3d(0px, 0px, 0px)');
-		$('.section').removeAttr('style');
-		$('.fp-tableCell').removeAttr('style');
-		$('.fp-section').removeAttr('style');
-		$('.fp-completley').removeAttr('style');
-	});
+	
 }); //////////////////////////////////////////////////////end document ready...
